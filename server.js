12f552274f7ea3271549ae1e628d80240209de97
shuzikicebot581 <eiainehaley@hotmail.com>
2025-08-31 09:57:53 -0500

    Update MongoDB connection string to use environment variable for database URL.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/plataforma-intercambio', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/plataforma-intercambio', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error(err));
 
