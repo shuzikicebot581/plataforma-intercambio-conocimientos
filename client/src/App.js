b1a0407c9118b127af37337e56c02cba7d761995
shuzikicebot581 <eiainehaley@hotmail.com>
2025-08-31 09:57:33 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenido a la Plataforma de Intercambio de Conocimientos</h1>
+      <p>Comienza a compartir y aprender con otros desarrolladores.</p>
+    </div>
+  );
+}
+
+export default App;
