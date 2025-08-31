0bb340949bcd47259d4148dda3584e61f3dc6f7a
shuzikicebot581 <eiainehaley@hotmail.com>
2025-08-31 09:57:29 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/plataforma-intercambio', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+  res.send('API de la Plataforma de Intercambio de Conocimientos');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
